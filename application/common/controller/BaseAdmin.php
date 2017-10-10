00c08afc8fbe5056f1cc7e620e3bebe62f95a134
大鱼 <clygd@126.com>
2017-10-11 00:08:01 +0800

    config

--- application/common/controller/BaseAdmin.php
+++ application/common/controller/BaseAdmin.php
@@ -10,11 +10,51 @@ namespace app\common\controller;
 use think\Controller;
 use think\Db;
 use think\db\Query;
+use app\common\service\DataService;
 
 class BaseAdmin extends Controller{
 
     public $title;
 
     public $table;
+
+    protected function _form($dbQuery = null, $tplFile = '', $pkField = '', $where = [], $extendData = [])
+    {
+        $db = is_null($dbQuery) ? Db::name($this->table) : (is_string($dbQuery) ? Db::name($dbQuery) : $dbQuery);
+        $pk = empty($pkField) ? ($db->getPk() ? $db->getPk() : 'id') : $pkField;
+        $pkValue = $this->request->request($pk, isset($where[$pk]) ? $where[$pk] : (isset($extendData[$pk]) ? $extendData[$pk] : null));
+        // 非POST请求, 获取数据并显示表单页面
+        if (!$this->request->isPost()) {
+            $vo = ($pkValue !== null) ? array_merge((array)$db->where($pk, $pkValue)->where($where)->find(), $extendData) : $extendData;
+            if (false !== $this->_callback('_form_filter', $vo)) {
+                empty($this->title) || $this->assign('title', $this->title);
+                return $this->fetch($tplFile, ['vo' => $vo]);
+            }
+            return $vo;
+        }
+        // POST请求, 数据自动存库
+        $data = array_merge($this->request->post(), $extendData);
+        if (false !== $this->_callback('_form_filter', $data)) {
+            $result = DataService::save($db, $data, $pk, $where);
+            if (false !== $this->_callback('_form_result', $result)) {
+                if ($result !== false) {
+                    $this->success('恭喜, 数据保存成功!', '');
+                }
+                $this->error('数据保存失败, 请稍候再试!');
+            }
+        }
+
+
+    }
+
+    protected function _callback($method, &$data)
+    {
+        foreach ([$method, "_" . $this->request->action() . "{$method}"] as $_method) {
+            if (method_exists($this, $_method) && false === $this->$_method($data)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
