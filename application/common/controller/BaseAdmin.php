e64a6d3e294b9b869d85c6e573527145e7bba2c2
clygd <clygd@126.com>
2017-10-10 16:16:40 +0800

    common

---
+++ application/common/controller/BaseAdmin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenly
+ * Date: 2017/10/10
+ * Time: 16:15
+ */
+namespace app\common\controller;
+
+use think\Controller;
+use think\Db;
+use think\db\Query;
+
+class BaseAdmin extends Controller{
+    
+}
