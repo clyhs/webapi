a2e2e34a48693c90464f996a95fcfce831de5bed
clygd <clygd@126.com>
2017-10-10 09:34:06 +0800

    think

--- build.php
+++ build.php
@@ -13,13 +13,28 @@ return [
     // 生成应用公共文件
     '__file__' => ['common.php', 'config.php', 'database.php'],
 
-    // 定义demo模块的自动生成 （按照实际定义的文件名生成）
-    'demo'     => [
+    //公共模块目录
+    'common' => [
         '__file__'   => ['common.php'],
-        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
-        'controller' => ['Index', 'Test', 'UserType'],
-        'model'      => ['User', 'UserType'],
-        'view'       => ['index/index'],
+        '__dir__'    => ['controller', 'model','lang'],
+        'controller' => ['Index'],
+        'model'      => ['Base'],
     ],
     // 其他更多的模块定义
+    // Admin 模块
+    'admin'     => [
+        '__file__'   => ['common.php'],
+        '__dir__'    => ['behavior', 'controller', 'model', 'view','lang'],
+        'controller' => ['Index'],
+        'model'      => ['Test'],
+        'view'       => ['index/index'],
+    ],
+    // Index模块
+    'index'     => [
+        '__file__'   => ['common.php'],
+        '__dir__'    => ['behavior', 'controller', 'model', 'view','lang'],
+        'controller' => ['Index'],
+        'model'      => ['Test'],
+        'view'       => ['index/index'],
+    ],
 ];
