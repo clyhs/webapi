ee8c8914f94119da356a805765d59a93407d3367
clygd <clygd@126.com>
2017-10-11 11:16:01 +0800

    common.php

--- application/admin/common.php
+++ application/admin/common.php
@@ -1,24 +1,2 @@
 <?php
 
-use think\Db;
-use app\common\service\DataService;
-use app\common\service\NodeService;
-
-function sysconf($name, $value = null)
-{
-    static $config = [];
-    if ($value !== null) {
-        list($config, $data) = [[], ['name' => $name, 'value' => $value]];
-        return DataService::save('config', $data, 'name');
-    }
-    if (empty($config)) {
-        $config = Db::name('config')->column('name,value');
-    }
-    return isset($config[$name]) ? $config[$name] : '';
-}
-
-function auth($node)
-{
-    //return NodeService::checkAuthNode($node);
-    return true;
-}
--- application/common.php
+++ application/common.php
@@ -10,3 +10,29 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+use think\Db;
+use app\common\service\DataService;
+
+function sysconf($name, $value = null)
+{
+    static $config = [];
+    if ($value !== null) {
+        list($config, $data) = [[], ['name' => $name, 'value' => $value]];
+        return DataService::save('config', $data, 'name');
+    }
+    if (empty($config)) {
+        $config = Db::name('config')->column('name,value');
+    }
+    return isset($config[$name]) ? $config[$name] : '';
+}
+
+function auth($node)
+{
+    //return NodeService::checkAuthNode($node);
+    return true;
+}
+
+function format_datetime($date, $format = 'Y年m月d日 H:i:s')
+{
+    return empty($date) ? '' : date($format, strtotime($date));
+}
