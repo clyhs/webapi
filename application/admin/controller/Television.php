19269f55f8507dac9c7a4d3c5e080de9b87ad6d0
clygd <clygd@126.com>
2017-10-12 09:28:54 +0800

    television

--- application/admin/controller/Television.php
+++ application/admin/controller/Television.php
@@ -18,4 +18,40 @@ class Television extends BaseAdmin{
         return view('', ['title' => '电台管理']);
     }
 
+    public function add()
+    {
+        $get = $this->request->get();
+
+
+        $country = "100000";
+        $countrys = Db::name("region")->where("code",$country)->order('code asc')->select();
+        $this->assign('countrys', $countrys);
+        $this->assign('country', $country);
+
+        if(isset($get['province']) && $get['province'] !== ''){
+            $province = $get['province'];
+            $provinces = Db::name("region")->where("code",$province)->order('code asc')->select();
+            $this->assign('provinces', $provinces);
+            $this->assign('province', $province);
+        }else{
+            $this->assign('provinces', "");
+        }
+        if(isset($get['city']) && $get['city'] !== ''){
+            $city = $get['city'];
+            $citys = Db::name("region")->where("code",$city)->order('code asc')->select();
+            $this->assign('citys', $citys);
+            $this->assign('city', $city);
+        }else{
+            $this->assign('citys', "");
+        }
+
+        return $this->_form($this->table, 'form');
+    }
+
+    public function getchildregion($parentCode){
+        $db = Db::name($this->table)->where("parentCode",$parentCode)->order('code asc');
+        $data = $db->select();
+        return json($data);
+    }
+
 }
