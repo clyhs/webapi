29c26fd4290a5113d10cd2c0951b0355f0b91e0a
clygd <clygd@126.com>
2017-10-13 17:54:23 +0800

    api

--- application/api/controller/Comment.php
+++ application/api/controller/Comment.php
@@ -43,10 +43,10 @@ class Comment extends Rest{
         $lists = $db->all();
         foreach ($lists as $key => &$item) {
             if($item['id']>0) {
-                $childrens = Db::field('a.*,b.username')
+                $childrens = Db::field('a.*')
                     ->table("t_comment")
                     ->alias('a')
-                    ->join('t_user b', 'b.id=a.reply_id')
+                    //->join('t_user b', 'b.id=a.reply_id')
                     ->where("a.pid="+$item['id'])
                     ->order('a.id desc');
                 $item['childrens'] = $childrens->select();
