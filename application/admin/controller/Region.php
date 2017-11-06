955e795c2d37dee657beb3a227bfd81e8cae1093
clygd <clygd@126.com>
2017-11-06 17:28:08 +0800

    region

--- application/admin/controller/Region.php
+++ application/admin/controller/Region.php
@@ -77,6 +77,7 @@ class Region extends BaseAdmin{
             $_menus = Db::name($this->table)->order('code asc')->select();
             $_menus[] = ['name' => '顶级地区', 'code' => '0', 'parentCode' => '0'];
             $menus = ToolService::arr2table($_menus,'code','parentCode');
+            /*
             foreach ($menus as $key => &$menu) {
                 if (substr_count($menu['path'], '-') > 3) {
                     unset($menus[$key]);
@@ -88,12 +89,11 @@ class Region extends BaseAdmin{
                         unset($menus[$key]);
                     }
                 }
-            }
+            }*/
             // 读取系统功能节点
 
             $this->assign('menus', $menus);
 
-            return json($menus);
         }
     }
 }
