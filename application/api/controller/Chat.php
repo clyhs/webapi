687517bfa219de760a0116ee343838fd6d4fa06e
clygd <clygd@126.com>
2017-11-10 16:20:06 +0800

    chat

--- application/api/controller/Chat.php
+++ application/api/controller/Chat.php
@@ -50,7 +50,7 @@ class Chat extends BaseApiRest
         $result = DataService::save($db, $data, $pk, []);
         $chat_id = Db::name($this->table)->getLastInsID();
 
-        if($filecount > 0 && $chat_id >0){
+        if($filecount > 0 && $result){
             $config = [
                 'exts'=>['mp4','jpg','png'],
                 'rootPath'=> $rootpath,
@@ -60,17 +60,31 @@ class Chat extends BaseApiRest
             $upload = new Upload($config,'LOCAL');
             $info   =   $upload->upload();
             //return json(["code"=>10000,"desc"=>"上传成功","data"=>$info]);
-            if($info){
+            if($info && $chat_id>0){
                 $info_num = count($info);
                 for($i=1;$i<$info_num+1;$i++){
 
                     $f = $info["file_$i"];
+                    $filename = $f["savename"];
+                    $filepath = $f["savepath"];
+                    $size = $f["size"];
+                    $url =  FileService::getBaseUriLocal().$filepath.$filename;
+                    $chat_info = [
+                        'chat_id'=>$chat_id,
+                        'url'=>$url,
+                        'size'=>$size
+                    ];
+                    $chat_info_db = Db::name("chat_info");
+                    $chat_result = DataService::save($db, $data, "", []);
                 }
+                return json(["code"=>10000,"desc"=>"成功"]);
+            }else{
+                return json(["code"=>20002,"desc"=>"文件没有上传成功"]);
             }
 
         }
 
-        return json(["code"=>10000,"desc"=>"上传成功","data"=>$info]);
+        return json(["code"=>10000,"desc"=>"保存成功"]);
 
     }
 
