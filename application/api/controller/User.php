7e8d5d25e3712db11c81f3dc061acf9c0899f2bf
大鱼 <clygd@126.com>
2017-11-18 22:52:15 +0800

    login api

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -106,10 +106,10 @@ class User extends BaseApiRest{
 
         $sql = 'select count(1) as history from t_user_tv where user_id='.$vo['id'].' '.
                ' and type_id=15 ';
-        $vo['history'] =Db::query($sql)['history'];
+        $vo['history'] =Db::query($sql)['0']['history'];
         $sql = 'select count(1) as likenum from t_user_tv where user_id='.$vo['id'].' '.
                ' and type_id=14 ';
-        $vo['likenum'] =Db::query($sql)['likenum'];
+        $vo['likenum'] =Db::query($sql)['0']['likenum'];
         return $vo;
     }
 
