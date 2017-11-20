217d8e9586b77fcc8a1e90878d7665ebd2d3c88d
clygd <clygd@126.com>
2017-11-20 16:59:51 +0800

    api

--- application/api/controller/Television.php
+++ application/api/controller/Television.php
@@ -133,20 +133,20 @@ class Television extends Rest{
             ->join(' t_region b ',' a.country = b.code ','left')
             ->join(' t_region c ',' a.province = c.code ','left')
             ->join(' t_dict d','FIND_IN_SET(d.id , a.type_ids) ','left')
-            ->where($hot)
+            //->where($hot)
             ->group('a.id')
             ->order('a.hit desc')
-            ->limit(4)->select();
+            ->limit(8)->select();
         $lists_new = Db::field('a.*,b.name as countryName,c.name as provinceName,GROUP_CONCAT(d.name) AS typeNames')
             ->table("t_television")
             ->alias('a')
             ->join(' t_region b ',' a.country = b.code ','left')
             ->join(' t_region c ',' a.province = c.code ','left')
             ->join(' t_dict d','FIND_IN_SET(d.id , a.type_ids) ','left')
-            ->where($new)
+            //->where($new)
             ->group('a.id')
             ->order('a.id desc')
-            ->limit(4)->select();
+            ->limit(8)->select();
         $lists_recommend = Db::field('a.*,b.name as countryName,c.name as provinceName,GROUP_CONCAT(d.name) AS typeNames')
             ->table("t_television")
             ->alias('a')
