e5301d9170ba0098dd8f77da37db6d2656188ab0
clygd <clygd@126.com>
2018-02-01 10:29:15 +0800

    tvp

---
+++ application/admin/controller/TVProgram.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenly
+ * Date: 2018/2/1
+ * Time: 10:02
+ */
+namespace app\admin\controller;
+
+use app\common\controller\BaseAdmin;
+use think\Db;
+use app\common\service\DataService;
+
+class TVProgram extends BaseAdmin{
+    public $table = 'television_program';
+
+    public function index(){
+
+        $this->title = '电视台节目管理';
+
+        $db = Db::field('a.*,b.name ')
+            ->table("t_television_program")
+            ->alias('a')
+            ->join(' t_television b ',' a.tv_id = b.id ','left');
+        $db->order('a.id desc');
+
+
+        return parent::_list($db);
+
+    }
+
+}
