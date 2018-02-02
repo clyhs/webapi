46929e4de51bc6095ae5c4ef3def230f54347f53
clygd <clygd@126.com>
2018-02-02 13:38:24 +0800

    api

--- application/route.php
+++ application/route.php
@@ -39,8 +39,8 @@ Route::get('api/tv/record/:userId/:typeId/[:page]/[:pageSize]','api/Television/g
 Route::post('api/tv/delrecord','api/Television/deleteTvByUserIdAndTypeId');
 Route::get('api/tv/all','api/Television/getalltv');
 Route::post('api/tv/collect/:type_id/:userId/:tv_id','api/Television/addTVByUserId');
-Route::get('api/tv/program/:date/:class','api/Television/getProgram');
-//Route::get('api/tv/programbyid/:id/:date','api/Television/getProgramById');
+//Route::get('api/tv/program/:date/:class','api/Television/getProgram');
+Route::get('api/tv/programbyid/:id/:date','api/Television/getProgramById');
 //Route::get('api/tv/programbytype/:class/:date','api/Television/getProgramForType');
 
 /**
