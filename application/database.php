1b9ad57e5f2211d9541408e3c6718b10af8bc75e
ABigfish <clygd@126.com>
2018-05-10 22:54:40 +0800

    Update database.php

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '47.91.156.179',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'webapi',
     // 用户名
