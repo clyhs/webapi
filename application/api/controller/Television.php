7d7c4684ab59365fac49261d1199c4a95051af98
clygd <clygd@126.com>
2018-05-29 10:17:50 +0800

    api

--- application/api/controller/Television.php
+++ application/api/controller/Television.php
@@ -867,7 +867,7 @@ class Television extends Rest{
         $error = curl_error($ch);
         curl_close($ch);
         if($error) throw new Exception('请求发生错误：' . $error);
-        return $data;
+        return json_decode($data,true);
     }
 
     private function http_post_json($url, $params)
