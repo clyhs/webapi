4dd1c15aa22a00086115afb8208d92cb09257159
clygd <clygd@126.com>
2018-05-29 10:11:59 +0800

    api

--- application/api/controller/Television.php
+++ application/api/controller/Television.php
@@ -884,6 +884,7 @@ class Television extends Rest{
                 'Content-Type: application/json; charset=utf-8'
             )
         );
+        curl_setopt($ch, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1);
         curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 5.2; rv:19.0) Gecko/20100101 Firefox/19.0");
         $response = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
