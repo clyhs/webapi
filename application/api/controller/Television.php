5bb59cef640dddc7209691bf7f894aea39907b99
clyhs <clygd@126.com>
2018-06-02 11:32:30 +0800

    api

--- application/api/controller/Television.php
+++ application/api/controller/Television.php
@@ -819,7 +819,7 @@ class Television extends Rest{
         $lists = Db::field(' max(a.play_at),a.play_time,a.tv_id,a.title')
             ->table("t_television_program")
             ->alias('a')
-            ->where(' now() between str_to_date(CONCAT(a.play_date," ",a.play_time,":00")) and str_to_date(CONCAT(a.play_date," ",a.end_time,":00"))')
+            ->where(' now() between str_to_date(CONCAT(a.play_date," ",a.play_time,":00"),"%Y%m%d %H:%i:%s %p") and str_to_date(CONCAT(a.play_date," ",a.end_time,":00"),"%Y%m%d %H:%i:%s %p")')
             ->group('a.tv_id')
             ->order(' rand() ')
             ->limit(10)->select();
