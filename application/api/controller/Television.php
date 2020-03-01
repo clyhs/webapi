9e83eb26bdd335c00beae7d648da448217a14887
clyhs <clygd@126.com>
2020-03-01 23:03:16 +0800

    api

--- application/api/controller/Television.php
+++ application/api/controller/Television.php
@@ -510,14 +510,14 @@ class Television extends Rest{
 
         $lists = Db::Table("t_television")
             ->alias('a')
-            ->field("a.*,b.name as countryName,c.name as provinceName,
+            ->field("a.*,b.name as countryName,c.name as provinceName,GROUP_CONCAT(d.name) AS typeNames,
         (select count(1) from t_comment e where e.uid=a.id and e.type_id=11 and e.pid=0 ) as commentNum,
         (select sum(good) from t_good_log f where f.uid=a.id and f.type_id=11 ) as goodNum ")
             //->table("t_television")
             //->alias('a')
             ->join(' t_region b ',' a.country = b.code ','left')
             ->join(' t_region c ',' a.province = c.code ','left')
-            // ->join(' t_dict d',Db::raw("FIND_IN_SET(d.id , a.type_ids)"),'left')
+            ->join(' t_dict d',Db::raw("FIND_IN_SET(d.id , a.type_ids)"),'left')
             ->where($where)
             ->group('a.id')
             //->order(' rand() ')
