dea4098e26a4bd2a40706eba948467d9a37edaaa
clyhs <clygd@126.com>
2020-03-01 19:19:52 +0800

    db

--- application/database.php
+++ application/database.php
@@ -19,7 +19,7 @@ return [
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'wqda@%12',
+    'password'        => 'Wqda@%12',
     // 端口
     'hostport'        => '',
     // 连接dsn
